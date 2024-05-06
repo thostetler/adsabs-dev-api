7d3a82bf11b24f5cf77137c673dc7fba0cfaf21a
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2024-05-06 13:00:56 -0400

    update build script to output json as well

--- build.sh
+++ build.sh
@@ -7,9 +7,12 @@ OUTPUT_DIR="./openapi/public"
 
 # bundle the public spec
 mkdir -p $TMP_DIR
-redocly bundle $PUBLIC_SPEC -o $TMP_DIR/$BUNDLE_NAME --ext yaml
-redocly split $TMP_DIR/$BUNDLE_NAME --outDir $OUTPUT_DIR
-redocly bundle $OUTPUT_DIR/openapi.yaml -o $OUTPUT_DIR/$BUNDLE_NAME --ext yaml
+npx @redocly/cli bundle $PUBLIC_SPEC -o $TMP_DIR/$BUNDLE_NAME --ext yaml
+npx @redocly/cli split $TMP_DIR/$BUNDLE_NAME --outDir $OUTPUT_DIR
+echo "making yaml bundle"
+npx @redocly/cli bundle $OUTPUT_DIR/openapi.yaml -o $OUTPUT_DIR/$BUNDLE_NAME --ext yaml
+echo "making json bundle"
+npx @redocly/cli bundle $OUTPUT_DIR/openapi.yaml -o $OUTPUT_DIR/$BUNDLE_NAME --ext json
 
 # clean up
 rm -rf $TMP_DIR
